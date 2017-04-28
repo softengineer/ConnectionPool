a73c8f72c6d2e913cb661c1e7fe998138aa66bd0
David.Fan <softengineer@users.noreply.github.com>
2017-04-29 10:21:38 +1200

    Update ConnectionPoolImpl.java
    update description

--- src/main/java/com/magicfan/ConnectionPoolImpl.java
+++ src/main/java/com/magicfan/ConnectionPoolImpl.java
@@ -19,30 +19,7 @@ import java.util.logging.Logger;
 /**
  * Connection pool implementation.
  *
- * This implementation should:
- *
- * - Use the provided ConnectionFactory implementation to build new
- *   Connection objects.
- * - Allow up to {@code maxConnections} simultaneous connections
- *   (both in-use and idle)
- * - Call Connection.testConnection() before returning a Connection to a
- *   caller; if testConnection() returns false, this Connection
- *   instance should be discarded and a different Connection obtained.
- * - Be safe to use by multiple callers simultaneously from different threads
- *
- * You may find the locking and queuing objects provided by
- * java.util.concurrent useful.
- *
- * Some possible extensions:
- *
- * - Check that connections returned via releaseConnection() were actually
- *   allocated via getConnection() (and haven't already been returned)
- * - Test idle connections periodically, and discard those which fail a
- *   testConnection() check.
- * - Detect Connections that have been handed out to a caller, but where the
- *   caller has discarded the Connection object, and don't count them as
- *   "in use". (hint: have the pool store WeakReferences to in-use connections,
- *   and use that to detect when they become only weakly reachable)
+ * 
  *
  */
 public class ConnectionPoolImpl implements ConnectionPool {
